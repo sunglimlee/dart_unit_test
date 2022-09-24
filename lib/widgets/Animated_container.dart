c103e3bf742b643c7a1581cf7287a856b9a63bbf
Steve Lee <steve.patriot@gmail.com>
2022-09-24 14:00:31 -0400

    TabBar, DefaultTabController, TabBarView - TabBar 이용해서 TabBarView 보여주기

--- lib/widgets/Animated_container.dart
+++ lib/widgets/Animated_container.dart
@@ -11,20 +11,37 @@ class AnimatedContainerWidget extends StatefulWidget {
 class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
   bool pressed = false;
 
+
+
   @override
   Widget build(BuildContext context) {
-    double height = pressed ? 150 : 250;
-    Color color = pressed ? Colors.orangeAccent : Colors.green;
+    //double height = pressed ? 150 : 250;
+    //Color color = pressed ? Colors.orangeAccent : Colors.green;
 
-    return Scaffold( // AnimatedContainer 를 사용하기 위해서는 반드시 Scaffold 로 감싸야 한다.
-      body: Center(
-        child: Container(child:
-          const Chip(
-            autofocus: true,
-            avatar: Icon(Icons.accessible_forward),
-            label: Text('Steve.patriot@gmail.com '),
-          ),),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold( // AnimatedContainer 를 사용하기 위해서는 반드시 Scaffold 로 감싸야 한다.
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.accessible_forward)),
+              Tab(icon: Icon(Icons.add),),
+              Tab(icon: Icon(Icons.account_circle_rounded),),
+            ],
+          ),
+          title: const Text('Sunglim Lee'),
+        ),
+        body: const Center(
+          child: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          )
+        ),
       ),
     );
   }
 }
+
