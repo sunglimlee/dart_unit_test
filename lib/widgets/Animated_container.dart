1f6d40ff42fa14e5aeb9606ac8584f21a6728778
Steve Lee <steve.patriot@gmail.com>
2022-09-24 13:12:26 -0400

    Chip - 자동으로 둥근모양의 버턴스타일을 만들어주네.. - 아이콘과 텍스트 합쳐서

--- lib/widgets/Animated_container.dart
+++ lib/widgets/Animated_container.dart
@@ -17,27 +17,14 @@ class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
     Color color = pressed ? Colors.orangeAccent : Colors.green;
 
     return Scaffold( // AnimatedContainer 를 사용하기 위해서는 반드시 Scaffold 로 감싸야 한다.
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-        InkWell(
-          onTap: () => setState(() => pressed = !pressed),
-          child: Center(
-            child: AnimatedContainer(
-              duration: const Duration(seconds: 1),
-              color: color,
-              height: height,
-              width: 250,
-              child: Hero(
-                tag: "hero1",
-                child: Image.network(
-                    'https://i.natgeofe.com/n/3faa2b6a-f351-4995-8fff-36d145116882/domestic-dog_16x9.jpg'), // 이 테그로 여기 위젯을 구분해주는구나. 같은 이름으로 하면 에니메이션 효과 설정이 가능하네..
-              ),
-            ),
-          ),
-        ),
-            Container(color: Colors.pink, width: 300, height: 300, child:
-              const FittedBox(child: Text('a'),),),
-      ]),
+      body: Center(
+        child: Container(child:
+          const Chip(
+            autofocus: true,
+            avatar: Icon(Icons.accessible_forward),
+            label: Text('Steve.patriot@gmail.com '),
+          ),),
+      ),
     );
   }
 }
